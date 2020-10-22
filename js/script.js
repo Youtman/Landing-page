eadae8e4fc9185a9db9c22dc7ba32ef6fb465181
Kevin Ochanda <kevochanda@gmail.com>
2020-10-22 18:20:55 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(() => {
+  $('.form-group').each((i, e) => {
+    $('.form-control', e)
+      .focus(function () {
+        e.classList.add('not-empty');
+      })
+      .blur(function () {
+        this.value === '' ? e.classList.remove('not-empty') : null;
+      });
+  });
+});
